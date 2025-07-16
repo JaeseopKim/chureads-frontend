38a0fe4abe9f865a81a8616e22df2fef04a347c2
skyrain22 <skyklouis@gmail.com>
2025-07-16 14:26:16 +0900

    map 추가

--- src/pages/Sample.jsx
+++ src/pages/Sample.jsx
@@ -1,21 +1,30 @@
 //rafce 축약 입력 시 리액트 기본코드 작성
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Greeting from '../components/sample/Greeting'
 
 const Sample = () => {
     // logic
-    //const [userName, setUserName] = useState("");
-
     const handleButtonClick = (data) => {
         console.log("click!!", data);
     }
+
+    const [userNames, setUserNames] = useState([]);
+
+    useEffect(() => {
+        // 컴포넌트 생성시 딱 한번만 실행
+        const nameDatas = ["하경아", "최선영", "송의진"];
+        setUserNames(nameDatas);
+
+    }, []);
+
+
     // view
     return (
         <div>Sample
-            <Greeting name="하경아" onButtonclick={handleButtonClick} />
-            <Greeting name="최선영" onButtonclick={handleButtonClick} />
-            <Greeting onButtonclick={handleButtonClick} />
+            {userNames.map((userName, index) =>
+                <Greeting key={`userName${index}`} name={userName} onButtonclick={handleButtonClick} />
+            )}
         </div>
     )
 }
